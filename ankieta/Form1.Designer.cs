877ca6de17a056f023ca80f2e6e92a9a1b9bdec4
Fabian Błaszak <f93bla@hotmail.com>
2021-11-04 16:11:41 +0100

    Add files via upload

---
+++ ankieta/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace ankieta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxPytanie = new System.Windows.Forms.GroupBox();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBoxPytanie.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxPytanie
+            // 
+            this.groupBoxPytanie.Controls.Add(this.radioButton3);
+            this.groupBoxPytanie.Controls.Add(this.radioButton2);
+            this.groupBoxPytanie.Controls.Add(this.radioButton1);
+            this.groupBoxPytanie.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxPytanie.Name = "groupBoxPytanie";
+            this.groupBoxPytanie.Size = new System.Drawing.Size(294, 99);
+            this.groupBoxPytanie.TabIndex = 0;
+            this.groupBoxPytanie.TabStop = false;
+            this.groupBoxPytanie.Text = "groupBox1";
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(232, 118);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 0;
+            this.buttonNext.Text = "Dalej";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(151, 118);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.Text = "Wstecz";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 155);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.groupBoxPytanie);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxPytanie.ResumeLayout(false);
+            this.groupBoxPytanie.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxPytanie;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ ankieta/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ankieta
+{
+    public partial class Form1 : Form
+    {
+        private List<string> pytania = new List<string>();
+        private List<string> odpowiedzi = new List<string>();
+        private int aktualnePytanie;
+
+        private void Pytanie()
+        {
+            if (aktualnePytanie == pytania.Count - 1)
+            {
+                buttonNext.Text = "Koniec";
+            }
+            else
+            {
+                buttonNext.Text = "Dalej";
+            }
+            if (aktualnePytanie == 0)
+            {
+                buttonBack.Enabled = false;
+            }
+            else
+            {
+                buttonBack.Enabled = true;
+            }
+            string[] pytanie = pytania[aktualnePytanie].Split(';');
+            groupBoxPytanie.Text = $"Pytanie {aktualnePytanie + 1} - {pytanie[0]}";
+            radioButton1.Text = pytanie[1];
+            radioButton2.Text = pytanie[2];
+            radioButton3.Text = pytanie[3];
+            try
+            {
+                switch (odpowiedzi[aktualnePytanie])
+                {
+                    case "a":
+                        radioButton1.Checked = true;
+                        radioButton2.Checked = false;
+                        radioButton3.Checked = false;
+                        break;
+                    case "b":
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = true;
+                        radioButton3.Checked = false;
+                        break;
+                    case "c":
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = false;
+                        radioButton3.Checked = true;
+                        break;
+                }
+            }
+            catch
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+            }
+                    
+        }
+
+        public Form1()
+        {
+            string executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string fileLocation = Path.Combine(executableLocation, "plik.txt");
+            foreach (string line in File.ReadLines(fileLocation))
+            {
+                if (line == "---") break;
+                pytania.Add(line);
+            }
+            aktualnePytanie = 0;
+            InitializeComponent();
+            Pytanie();
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                odpowiedzi.Insert(aktualnePytanie, "a");
+            }
+            else if (radioButton2.Checked == true)
+            {
+                odpowiedzi.Insert(aktualnePytanie, "b");
+            }
+            else if (radioButton3.Checked == true)
+            {
+                odpowiedzi.Insert(aktualnePytanie, "c");
+            } 
+
+            if (aktualnePytanie == pytania.Count - 1)
+            {
+                Podsumowanie();
+            }
+            else
+            {
+                aktualnePytanie += 1;
+                Pytanie();
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            aktualnePytanie -= 1;
+            Pytanie();
+        }
+
+        private void Podsumowanie()
+        {
+            string odpowiedzi = String.Join("", this.odpowiedzi.ToArray());
+            string executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string fileLocation = Path.Combine(executableLocation, "plik.txt");
+            string[] klucz = File.ReadLines(fileLocation).Last().Split(';');
+            int[] odpowiedzicount = { odpowiedzi.Split('a').Length - 1, odpowiedzi.Split('b').Length - 1, odpowiedzi.Split('c').Length - 1, odpowiedzi.Split('d').Length - 1 }; // policz ile jest w odpowiedziach odpowiednio a b c d
+
+
+            var indeksymaksow = odpowiedzicount.Select((n, i) => new { n, i })
+                                    .GroupBy(x => x.n, x => x.i)
+                                    .OrderByDescending(x => x.Key)
+                                    .Take(1)
+                                    .SelectMany(x => x)
+                                    .ToArray();
+
+            foreach (var i in indeksymaksow)
+            {
+                Console.WriteLine(klucz[i]);
+            }
+        }
+    }
+}
